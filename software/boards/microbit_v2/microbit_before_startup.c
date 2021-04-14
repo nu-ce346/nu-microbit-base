030994c26f51fe9f22bc436dbd7096cf02dbbd30
Branden Ghena <brghena@berkeley.edu>
2021-04-14 11:05:19 -0500

    Add delay to slow reset
    Holding the reset button triggers a restart every few milliseconds.
    Delaying 10 ms once before calling `main()` means that the main function
    doesn't start if the reset button is held. Which is more the expected
    behavior. On reset this wastes time, but only 10 ms, so it seems safe.

--- software/boards/microbit_v2/microbit_before_startup.c
+++ software/boards/microbit_v2/microbit_before_startup.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 
+#include "nrf_delay.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 
@@ -18,5 +19,9 @@ void called_first (void) {
   ret_code_t error_code = NRF_LOG_INIT(NULL);
   APP_ERROR_CHECK(error_code);
   NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+  // holding reset restarts repeatedly
+  // this brief delay means `main()` only runs when reset is released
+  nrf_delay_ms(10);
 }
 
