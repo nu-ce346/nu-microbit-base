5c00527392de68ac266aac938c7c7004c50d9c8e
Branden Ghena <brghena@berkeley.edu>
2021-05-14 09:42:26 -0500

    I2C sensors app

---
+++ software/apps/i2c_sensors/main.c
@@ -0,0 +1,40 @@
+// I2C sensors app
+//
+// Read from I2C accelerometer/magnetometer on the Microbit
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "nrf_delay.h"
+#include "nrf_twi_mngr.h"
+
+#include "microbit_v2.h"
+#include "lsm303agr.h"
+
+// Global variables
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 1, 0);
+
+int main(void) {
+  printf("Board started!\n");
+
+  // Initialize I2C peripheral and driver
+  nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  i2c_config.scl = I2C_SCL;
+  i2c_config.sda = I2C_SDA;
+  i2c_config.frequency = NRF_TWIM_FREQ_100K;
+  nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+
+  // Initialize the LSM303AGR accelerometer/magnetometer sensor
+  lsm303agr_init(&twi_mngr_instance);
+
+  // Loop forever
+  while (1) {
+    // Print output
+    //TODO: implement me!
+
+    nrf_delay_ms(1000);
+  }
+}
+
