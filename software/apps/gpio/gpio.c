9a2aa30c4f4cfea16364ca32c58d399a5b539990
Branden Ghena <brghena@berkeley.edu>
2022-10-06 11:52:34 -0500

    Add better docs for GPIO instructions

--- software/apps/gpio/gpio.c
+++ software/apps/gpio/gpio.c
@@ -10,6 +10,7 @@ typedef struct{
 //  dir - gpio direction (INPUT, OUTPUT)
 void gpio_config(uint8_t gpio_num, gpio_direction_t dir) {
   // Implement me
+  // This function should configure the pin as an input/output
   // Hint: Use proper PIN_CNF instead of DIR
 }
 
@@ -17,12 +18,16 @@ void gpio_config(uint8_t gpio_num, gpio_direction_t dir) {
 //  gpio_num - gpio number 0-31 OR (32 + gpio number)
 void gpio_set(uint8_t gpio_num) {
   // Implement me
+  // This function should make the pin high
+  // It can assume that the pin has already been configured
 }
 
 // Inputs: 
 //  gpio_num - gpio number 0-31 OR (32 + gpio number)
 void gpio_clear(uint8_t gpio_num) {
   // Implement me
+  // This function should make the pin low
+  // It can assume that the pin has already been configured
 }
 
 // Inputs: 
@@ -31,13 +36,16 @@ void gpio_clear(uint8_t gpio_num) {
 //  bool - pin state (true == high)
 bool gpio_read(uint8_t gpio_num) {
   // Implement me
+  // This function should read the value from the pin
+  // It can assume that the pin has already been configured
 
   return true;
 }
 
 // prints out some information about the GPIO driver. Can be called from main()
 void gpio_print(void) {
-  // use this function for debugging purposes
-  // you don't otherwise have to write anything here
+  // Use this function for debugging purposes
+  // For example, you could print out struct field addresses
+  // You don't otherwise have to write anything here
 }
 
