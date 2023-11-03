aabe1e68fbfe127b0e89a2d9082f18b1216244f5
Branden Ghena <brghena@berkeley.edu>
2023-11-03 08:30:51 -0500

    Updates for lab6

--- software/apps/i2c_sensors/lsm303agr.c
+++ software/apps/i2c_sensors/lsm303agr.c
@@ -23,7 +23,17 @@ static uint8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr) {
   nrf_twi_mngr_transfer_t const read_transfer[] = {
     //TODO: implement me
   };
-  nrf_twi_mngr_perform(i2c_manager, NULL, read_transfer, 2, NULL);
+  ret_code_t result = nrf_twi_mngr_perform(i2c_manager, NULL, read_transfer, 2, NULL);
+  if (result != NRF_SUCCESS) {
+    // Likely error codes:
+    //  NRF_ERROR_INTERNAL            (0x0003) - something is wrong with the driver itself
+    //  NRF_ERROR_INVALID_ADDR        (0x0010) - buffer passed was in Flash instead of RAM
+    //  NRF_ERROR_BUSY                (0x0011) - driver was busy with another transfer still
+    //  NRF_ERROR_DRV_TWI_ERR_OVERRUN (0x8200) - data was overwritten during the transaction
+    //  NRF_ERROR_DRV_TWI_ERR_ANACK   (0x8201) - i2c device did not acknowledge its address
+    //  NRF_ERROR_DRV_TWI_ERR_DNACK   (0x8202) - i2c device did not acknowledge a data byte
+    printf("I2C transaction failed! Error: %lX\n", result);
+  }
 
   return rx_buf;
 }
@@ -46,43 +56,41 @@ void lsm303agr_init(const nrf_twi_mngr_t* i2c) {
   // ---Initialize Accelerometer---
 
   // Reboot acclerometer
-  i2c_reg_write(LSM303AGR_ACC_ADDRESS, LSM303AGR_ACC_CTRL_REG5, 0x80);
+  i2c_reg_write(LSM303AGR_ACC_ADDRESS, CTRL_REG5_A, 0x80);
   nrf_delay_ms(100); // needs delay to wait for reboot
 
   // Enable Block Data Update
   // Only updates sensor data when both halves of the data has been read
-  i2c_reg_write(LSM303AGR_ACC_ADDRESS, LSM303AGR_ACC_CTRL_REG4, 0x80);
+  i2c_reg_write(LSM303AGR_ACC_ADDRESS, CTRL_REG4_A, 0x80);
 
   // Configure accelerometer at 100Hz, normal mode (10-bit)
   // Enable x, y and z axes
-  i2c_reg_write(LSM303AGR_ACC_ADDRESS, LSM303AGR_ACC_CTRL_REG1, 0x57);
+  i2c_reg_write(LSM303AGR_ACC_ADDRESS, CTRL_REG1_A, 0x57);
 
   // Read WHO AM I register
   // Always returns the same value if working
-  uint8_t result = i2c_reg_read(LSM303AGR_ACC_ADDRESS, LSM303AGR_ACC_WHO_AM_I_REG);
-  //TODO: check the result of the Accelerometer WHO AM I register
+  //TODO: read the Accelerometer WHO AM I register and check the result
 
   // ---Initialize Magnetometer---
 
   // Reboot magnetometer
-  i2c_reg_write(LSM303AGR_MAG_ADDRESS, LSM303AGR_MAG_CFG_REG_A, 0x40);
+  i2c_reg_write(LSM303AGR_MAG_ADDRESS, CFG_REG_A_M, 0x40);
   nrf_delay_ms(100); // needs delay to wait for reboot
 
   // Enable Block Data Update
   // Only updates sensor data when both halves of the data has been read
-  i2c_reg_write(LSM303AGR_MAG_ADDRESS, LSM303AGR_MAG_CFG_REG_C, 0x10);
+  i2c_reg_write(LSM303AGR_MAG_ADDRESS, CFG_REG_C_M, 0x10);
 
   // Configure magnetometer at 100Hz, continuous mode
-  i2c_reg_write(LSM303AGR_MAG_ADDRESS, LSM303AGR_MAG_CFG_REG_A, 0x0C);
+  i2c_reg_write(LSM303AGR_MAG_ADDRESS, CFG_REG_A_M, 0x0C);
 
   // Read WHO AM I register
-  result = i2c_reg_read(LSM303AGR_MAG_ADDRESS, LSM303AGR_MAG_WHO_AM_I_REG);
-  //TODO: check the result of the Magnetometer WHO AM I register
+  //TODO: read the Magnetometer WHO AM I register and check the result
 
   // ---Initialize Temperature---
 
   // Enable temperature sensor
-  i2c_reg_write(LSM303AGR_ACC_ADDRESS, LSM303AGR_ACC_TEMP_CFG_REG, 0xC0);
+  i2c_reg_write(LSM303AGR_ACC_ADDRESS, TEMP_CFG_REG_A, 0xC0);
 }
 
 // Read the internal temperature sensor
