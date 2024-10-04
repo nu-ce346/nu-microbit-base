df2e1faeac18337177349c9d6ef5a32da98af8f0
Branden Ghena <brghena@berkeley.edu>
2024-10-03 23:04:19 -0500

    Fix GCC warnings for F24

---
+++ software/boards/microbit_v2_minimal/clib_stubs.c
@@ -0,0 +1,14 @@
+// These are meaningless stub functions to satisfy the GCC linker
+
+void _close() {}
+
+void _fstat() {}
+
+void _getpid() {}
+
+void _isatty() {}
+
+void _kill() {}
+
+void _lseek() {}
+
