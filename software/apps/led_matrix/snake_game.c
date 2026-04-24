9c9904b942529247919b77a0ce9b7361f08fe09c
Branden Ghena <brghena@berkeley.edu>
2026-04-24 08:23:23 -0500

    Revise snake_game starter code

--- software/apps/led_matrix/snake_game.c
+++ software/apps/led_matrix/snake_game.c
@@ -55,60 +55,11 @@ static game_state_t game_state;
 
 
 // ***** Queue implementation *****
-// Implemented using a ring buffer
-// You shouldn't have to edit this
 
-// A queue that holds snakeloc_t structs
-// 25 slots is enough for a maximum-sized snake given our screen dimensions
-typedef struct {
-  snakeloc_t slots[25];
-  uint8_t head;
-  uint8_t tail;
-} queue_t;
-
-static queue_t queue;
-
-// Initializes the queue
-static void queue_init(void) {
-  for (int i=0; i<25; i++) {
-    queue.slots[i].row = -1;
-    queue.slots[i].col = -1;
-  }
-  queue.head = 0;
-  queue.tail = 0;
-}
-
-// Pushes an item to the head of the queue
-// Overwrites the tail of the queue if the queue is full
-static void queue_push_head(snakeloc_t position) {
-  // head points at empty slot, write there
-  queue.slots[queue.head] = position;
-
-  // advance head
-  queue.head++;
-  if (queue.head >= 25) {
-    queue.head = 0;
-  }
-}
-
-// Pops an item from the tail of the queue
-// Returns {-1, -1} if no item exists
-static snakeloc_t queue_pop_tail(void) {
-  // tail points at last element unless head and tail overlap, read there
-  snakeloc_t result = {-1, -1};
-  if (queue.tail != queue.head) {
-    result.row = queue.slots[queue.tail].row;
-    result.col = queue.slots[queue.tail].col;
-  }
-
-  // advance tail
-  queue.tail++;
-  if (queue.tail >= 25) {
-    queue.tail = 0;
-  }
-
-  return result;
-}
+// TODO
+// You will need init(), push(), and pop() functionality.
+// This is most reasonably implemented as a ring buffer using a 25-element
+// array, although other implementations are possible.
 
 // ********************************
 
@@ -138,8 +89,7 @@ void snake_game_init(void) {
 
   // initialize ring buffer
   // push initial head into the queue
-  queue_init();
-  queue_push_head(game_state.head);
+  // TODO
 
   // initialize button inputs
   // calls the `gpio_handler` callback function when either button is pressed
@@ -155,36 +105,6 @@ void snake_game_init(void) {
 // this is intended to be called repeatedly from the main code, with a delay between each call
 // each call advances the game state one iteration
 void snake_game_advance_state(void) {
-  // if the game is over, no longer advance the state
-  // TODO
-
-  // handle button presses
-  //  If BTN_A was pressed, rotate left from the current direction
-  //  If BTN_B was pressed, rotate right from the current direction
-  //    e.g., if previously going right, now go down instead
-  // TODO
-
-  // remove the snake tail from the queue and the model
-  //  unless the number of iterations is a multiple of ten,
-  //  in which case grow the snake by simply NOT removing the tail
-  // TODO
-
-  // advance the snake head location based on the current direction
-  // TODO
-
-  // check for collision with game boarders (<0 or >4) or with the snake itself
-  //  if the new head position was already true in the model, that's a collision
-  //
-  // In the case of a collision:
-  //  game_over should become true
-  //  print a message with the number of iterations (the current score)
-  //  draw or write something notable on the LED matrix so it's clear the game ended
-  // TODO
-
-  // update game state with new head location in the model and the queue
-  // TODO
-
-  // display the game model on the LED matrix
   // TODO
 }
 
